d40f15eaf90b97f542d1a120bd26de306d392e39
rory <you@example.com>
2021-12-06 07:51:26 +0000

    add my own playground and json output'

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -8,15 +8,27 @@ class ToScrapeCSSSpider(scrapy.Spider):
         'http://quotes.toscrape.com/',
     ]
 
+    # def parse(self, response):
+    #     for quote in response.css("div.quote"):
+    #         yield {
+    #             'text': quote.css("span.text::text").extract_first(),
+    #             'author': quote.css("small.author::text").extract_first(),
+    #             'tags': quote.css("div.tags > a.tag::text").extract()
+    #         }
+
+    #     next_page_url = response.css("li.next > a::attr(href)").extract_first()
+    #     if next_page_url is not None:
+    #         yield scrapy.Request(response.urljoin(next_page_url))
+
+    
+    # def parse_top_10_tags(self, response):
+    #     for tag in response.css("span.tag-item"):
+    #         yield {
+    #             'tags': tag.css("a.tag::text").extract_first()
+    #         }
+
     def parse(self, response):
-        for quote in response.css("div.quote"):
+        for tag in response.css("span.tag-item"):
             yield {
-                'text': quote.css("span.text::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.tags > a.tag::text").extract()
+                'tags': tag.css("a.tag::text").extract_first()
             }
-
-        next_page_url = response.css("li.next > a::attr(href)").extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
-
